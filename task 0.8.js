c269f8228ae54cf9ef52c03cd1fff5a6e95257a1
Andy Matobela <49431879+andymatobela@users.noreply.github.com>
2021-07-26 12:13:44 +0200

    Update task 0.8.js

--- task 0.8.js
+++ task 0.8.js
@@ -1,7 +1,23 @@
 function timeConversion(time){
     var hours = Math.floor(time / 60);
     var minutes = time % 60;
-
+    if (hours > 1 && minutes > 1){
     console.log(hours + " hours, " + minutes + " minutes"); 
+    }
+
+    else if (hours > 1 && minutes < 0){
+
+        console.log(hours + " hours, " + minutes + " minute"); 
+    }
+
+    else if (hours <= 1 && minutes > 1){
+
+            console.log(hours + " hour, " + minutes + " minutes"); 
+        }
+
+    else {
+        console.log(hours + " hour, " + minutes + " minute"); 
+
+    }
 }
-timeConversion(133);
+timeConversion(45);
